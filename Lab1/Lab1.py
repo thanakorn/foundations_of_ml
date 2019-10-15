1fbf3e702154c9781c8a188e666fb52338748ea1
thanakorn <thanakorn.panyapiang@outlook.com>
2019-10-15 14:38:21 +0100

    Move folder and download benchmark datasets

--- Lab1/Lab1.py
+++ Lab1/Lab1.py
@@ -1,5 +1,4 @@
 #%% COMP6245 - Lab 1
-
 #%% Import libraries
 %matplotlib inline
 import numpy as np
@@ -39,59 +38,84 @@ print(f'inv(B) : {inv_B}')
 print(f'B x B(-1) : {B @ inv_B}')
 print(f'Is B x B(-1) an identity matrix : {np.allclose(B @ inv_B, np.eye(B.shape[0]))}')
 
-eigvals, eigvecs = np.linalg.eig(B)
-eigvecs_t = eigvecs.T
-print(f'Eigenvalues of B : {eigvals}')
-print(f'Eigenvectors of B : {eigvecs}')
-print(f'Eigvecs x Eigvecs.T : {eigvecs @ eigvecs.T}')
+D, U = np.linalg.eig(B)
+eigvecs_t = U.T
+print(f'D : {D}')
+print(f'U : {U}')
+print(f'U[1] . U[2] : {np.dot(U[:,0], U[:,1])}')
+print(f'U x U.T : {U @ U.T}')
 
 #%% 2. Random Numbers and Univariate Distribution
-num_samples = 5000
-num_bins = 30
+num_trial = 5
+num_samples = [100, 1000, 5000]
+num_bins = [10, 20, 30, 50]
 bin_counts_var = np.array([], dtype = float)
 
-# for trial in range(1):
-#     x = np.random.rand(num_samples, 1)
-#     plt.figure(figsize=(4, 4,))
-#     #plt.title(label = f'Trial #{trial}')
-#     plt.xlabel('Bin')
-#     plt.ylabel('Counts')
-#     counts, bins, patches = plt.hist(x, bins = num_bins)
-#     plt.savefig('random_numbers')
-#     var = np.var(counts/num_samples)
-#     bin_counts_var = np.append(bin_counts_var, np.var(counts/num_samples))
-fig, subfig = plt.subplots(1, 5, figsize = (18,3))
-num_random = [2, 10, 50, 100, 1000]
-sum_var = np.zeros(5)
-for k in range(len(num_random)):
-    for trial in range(1):
-        x = np.zeros(num_samples)
-        for i in range(num_samples): x[i] = sum(np.random.rand(num_random[k], 1))
-        #plt.figure(figsize=(2, 2,))
-        #plt.xlabel('Bin')
-        #plt.ylabel('Counts')
-        subfig[k].hist(x, num_bins, alpha=0.8, rwidth=0.8)
-        sum_var[k] = np.var(x)
-        plt.savefig('sum_random_numbers')
-plt.figure(figsize=(3,3))
-plt.plot(num_random, sum_var)
+uniform_random_fig, uniform_random_subfig = plt.subplots(len(num_samples), len(num_bins), sharex=True, figsize=(len(num_samples) * 4,len(num_bins) * 1))
+for i in range(len(num_samples)):
+    for j in range(len(num_bins)):
+        x = np.random.rand(num_samples[i], 1)
+        counts, bins, patches = uniform_random_subfig[i,j].hist(x, bins = num_bins[j])
+        if(j == 0):
+             uniform_random_subfig[i,j].set(ylabel = f'{num_samples[i]} samples')
+        if(i == len(num_samples) - 1):
+            uniform_random_subfig[i,j].set(xlabel = f'{num_bins[j]} bins')
+        # var = np.var(counts/num_samples)
+        # bin_counts_var = np.append(bin_counts_var, np.var(counts/num_samples))
+plt.savefig('Lab1/uniform_random_numbers.png', bbox_inches='tight')
+
+num_samples_guassian = [100, 500, 2000, 5000]
+gaussian_random_fig, gaussian_random_subfig = plt.subplots(1, 4, figsize=(15,2))
+for i in range(len(num_samples_guassian)):
+    x = np.random.randn(num_samples_guassian[i], 1)
+    counts, bins, patches = gaussian_random_subfig[i].hist(x, bins = num_bins[0])
+    gaussian_random_subfig[i].set(xlabel = f'{num_samples_guassian[i]} samples')
+    # var = np.var(counts/num_samples)
+    # bin_counts_var = np.append(bin_counts_var, np.var(counts/num_samples))
+plt.savefig('Lab1/gaussian_random_numbers.png', bbox_inches='tight')
+
+sum_random_fig, sum_random_subfig = plt.subplots(1, 4, figsize=(15,2))
+num_randoms = [2, 5, 10, 25]
+num_samples_sum = [100, 500, 2000, 5000, 10000]
+for k in range(len(num_randoms)):
+    x = np.zeros(num_samples_sum[2])
+    for i in range(num_samples_sum[2]): 
+        x[i] = sum(np.random.rand(num_randoms[k], 1)) - sum(np.random.rand(num_randoms[k], 1))
+    counts, bins, patches = sum_random_subfig[k].hist(x, num_bins[-1])
+    sum_random_subfig[k].set(xlabel = f'sum of {num_randoms[k]} numbers')
+plt.savefig('Lab1/sum_random_numbers.png', bbox_inches='tight')
 
 #%% 3. Uncertainty in Estimation
 max_trial = 2000
-sample_size_range = np.linspace(100, 500, 40)
+sample_size_range = np.linspace(100, 500, 100)
+plot_var = np.zeros(len(sample_size_range))
+
+uncer_fig, uncer_subfig = plt.subplots(1,2, figsize=(8,2))
+for sample_size_index in range(len(sample_size_range)):
+    num_samples = np.int(sample_size_range[sample_size_index])
+    trail_vars = np.zeros(max_trial)
+    for trial in range(max_trial):
+        samples = np.random.randn(num_samples, 1) * 10
+        trail_vars[trial] = np.var(samples)
+    plot_var[sample_size_index] = np.var(trail_vars)
+uncer_subfig[0].title.set_text('Estimated Variance of\nGaussian Sampling')
+uncer_subfig[0].set_xlabel('#Samples')
+uncer_subfig[0].set_ylabel('Variance')
+uncer_subfig[0].plot(sample_size_range, plot_var)
+
 plot_var = np.zeros(len(sample_size_range))
 for sample_size_index in range(len(sample_size_range)):
     num_samples = np.int(sample_size_range[sample_size_index])
     trail_vars = np.zeros(max_trial)
     for trial in range(max_trial):
-        samples = np.random.randn(num_samples, 1)
+        samples = np.random.rand(num_samples, 1) * 10
         trail_vars[trial] = np.var(samples)
     plot_var[sample_size_index] = np.var(trail_vars)
-plt.title('Variance in Estimating Variance')
-plt.xlabel('#Samples')
-plt.ylabel('Variance')
-plt.plot(sample_size_range, plot_var)
-
+uncer_subfig[1].title.set_text('Estimated Variance of\nUniform Sampling')
+uncer_subfig[1].set_xlabel('#Samples')
+uncer_subfig[1].set_ylabel('Variance')
+uncer_subfig[1].plot(sample_size_range, plot_var)
+plt.savefig('Lab1/estmd_variance.png', bbox_inches='tight')
 #%% 4. Bivariate Gaussian Distribution
 def gauss2D(x, m, C):
     Ci = np.linalg.inv(C)
@@ -115,57 +139,85 @@ def twoDGaussianPlot(nx, ny, m, C):
 
 nx, ny = 50, 40
 m1 = np.array([0, 2])
-C1 = np.array([[2,1], [1,2]], np.float32)
-X1p, Y1p, Z1p = twoDGaussianPlot(nx, ny, m1, C1)
+C = np.array([[2,1], [1,2]], np.float32)
+X1p, Y1p, Z1p = twoDGaussianPlot(nx, ny, m1, C)
 m2 = np.array([2, 0])
 C2 = np.array([[2,-1], [-1,2]], np.float32)
 X2p, Y2p, Z2p = twoDGaussianPlot(nx, ny, m2, C2)
 m3 = np.array([-2, -2])
 C3 = np.array([[2,0], [0,2]], np.float32)
 X3p, Y3p, Z3p = twoDGaussianPlot(nx, ny, m3, C3)
-plt.contour(X1p, Y1p, Z1p, 3)
-plt.contour(X2p, Y2p, Z2p, 3)
-plt.contour(X3p, Y3p, Z3p, 3)
+plt.contour(X1p, Y1p, Z1p, 3, colors = 'r')
+plt.contour(X2p, Y2p, Z2p, 3, colors = 'g')
+plt.contour(X3p, Y3p, Z3p, 3, colors = 'b')
+plt.xlabel('X')
+plt.ylabel('Y')
 plt.grid(True)
+plt.title('Contour plot of 3 bivariate Gaussian densities')
+plt.savefig('Lab1/contours.png', bbox_inches='tight')
 
 #%% 5. Sampling from a Multivariate Gaussian Distribution
-C1 = [[2,1], [1,2]]
+C = [[2,1], [1,2]]
 C2 = [[2,-1], [-1,2]]
-A1 = np.linalg.cholesky(C1)
+A = np.linalg.cholesky(C)
 A2 = np.linalg.cholesky(C2)
 
 X = np.random.randn(10000, 2)
-Y1 = X @ A1
+Y = X @ A
 Y2 = X @ A2
 plt.title('Scatter of Isotropic and Correlated Gaussian Densities')
-plt.scatter(Y1[:,0], Y1[:,1], c='m', s=100, edgecolors='')
-# plt.scatter(Y2[:,0], Y2[:,1], c='r')
-plt.scatter(X[:,0], X[:,1], c='c', s=100, edgecolors='')
+plt.scatter(Y[:,0], Y[:,1], c='r', s=100, edgecolors='')
+plt.scatter(X[:,0], X[:,1], c='g', s=100, edgecolors='')
 plt.grid(True)
+plt.savefig('Lab1/scatter_of_isotropic.png', bbox_inches='tight')
 
 #%% 6. Distribution of Projections
-theta = np.pi/3
-u = [np.sin(theta), np.cos(theta)]
-print('Sum of squares', u[0]**2 + u[1]**2)
-print('Degrees : ', theta*180/np.pi)
-y1_proj = Y1 @ u
-y2_proj = Y2 @ u
+theta = np.pi / 3
+u = [np.cos(theta), np.sin(theta)]
+y_proj = Y @ u
+print('Dimension before projection : ', Y.shape)
+print('Dimension after projection : ', y_proj.shape)
+print('Original variance : ', np.var(Y))
+print('Projected variance : ', np.var(y_proj))
 
 num_theta = 360
-y1_proj_var = np.zeros(num_theta)
+y_proj_var = np.zeros(num_theta)
 y2_proj_var = np.zeros(num_theta)
 theta_range = np.linspace(0, 2 * np.pi, num_theta)
+max_var = 0.0
+max_u = [0.0, 0.0]
 for i in range(num_theta):
     theta = theta_range[i]
-    u = [np.sin(theta), np.cos(theta)]
-    y1_proj_var[i] = np.var(Y1 @ u)
+    u = [np.cos(theta), np.sin(theta)]
+    cur_var = np.var(Y @ u)
+    y_proj_var[i] = np.var(Y @ u)
     y2_proj_var[i] = np.var(Y2 @ u)
 plt.title('Variance of projections of Y according to theta')
 plt.grid(True)
 plt.xlabel('Theta')
+plt.xlim(xmin=0, xmax=360)
 plt.ylabel('Variance of Projection')
-plt.plot(y1_proj_var, c='m')
-plt.plot(y2_proj_var, c='c')
+plt.plot(y_proj_var, c='r', label = 'C=[[ 2, 1],[ 1, 2]]')
+plt.plot(y2_proj_var, c='g', label = 'C=[[ 2,-1],[-1, 2]]')
+plt.legend()
+plt.savefig('Lab1/variance_of_proj.png', bbox_inches='tight')
 
-y1_e_vals, y1_e_vecs = np.linalg.eig(C1)
-y2_e_vals, y2_e_vecs = np.linalg.eig(C2)
+y_evals, y_evecs = np.linalg.eig([[2,1], [1,2]])
+y_ev1 = y_evecs[:,0]
+y_ev2 = y_evecs[:,1]
+y2_evals, y2_evecs = np.linalg.eig([[2,-1], [-1,2]])
+y2_ev1 = y2_evecs[:,0]
+y2_ev2 = y2_evecs[:,1]
+print('Y eigenvalues : ', y_evals)
+print('Y eigenvector 1 : ', y_ev1)
+print('Y eigenvector 2 : ', y_ev2)
+print('Y2 eigenvalues : ', y2_evals)
+print('Y2 eigenvector 1 : ', y2_ev1)
+print('Y2 eigenvector 2 : ', y2_ev2)
+
+print('Y1 Eigenvector-proj max variance : ', np.var(Y @ y_ev1))
+print('Y1 Eigenvector-proj min variance : ', np.var(Y @ y_ev2))
+print('Y2 Eigenvector-proj max variance : ', np.var(Y2 @ y2_ev1))
+print('Y1 Eigenvector-proj min variance : ', np.var(Y @ y_ev1))
+
+#%%
--- Lab2/Lab2.py
+++ Lab2/Lab2.py
@@ -2,6 +2,7 @@
 %matplotlib inline
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
 #%% Generate Samples
 num_data_per_class = 200
@@ -24,61 +25,33 @@ plt.scatter(Y2[:,0], Y2[:,1], c = 'b', label = 'Y2')
 plt.legend()
 plt.savefig('scatter.png')
 #%% Generate training set and testing set
-samples = np.concatenate((Y1, Y2))
+Y = np.concatenate((Y1, Y2))
 label_positive = np.ones(num_data_per_class)
-label_negative = np.ones(num_data_per_class)
+label_negative = -1 * np.ones(num_data_per_class)
 labels = np.concatenate((label_positive, label_negative))
 
 sample_indices = np.random.permutation(num_data_per_class * 2)
-train_samples = samples[sample_indices, :]
-train_labels = labels[sample_indices]
+shuffle_samples = Y[sample_indices,:]
+shuffle_labels = labels[sample_indices]
 
-# Check accuracy
-def percent_correct(inputs, targets, weights):
-    n = len(targets)
+train_samples = shuffle_samples[0:num_data_per_class,:]
+train_labels = shuffle_labels[0:num_data_per_class]
+test_samples = shuffle_samples[num_data_per_class:2*num_data_per_class,:]
+test_labels = shuffle_labels[num_data_per_class:2*num_data_per_class]
+print('Training data : ', train_samples.shape)
+print('Test data : ', test_samples.shape)
+#%% Calculate accuracy
+def PercentCorrect(Inputs, targets, weights):
+    N = len(targets)
     num_correct = 0
-    for i in range(n):
-        input = inputs[i,:]
+    for i in range(N):
+        input = Inputs[i,:]
         if(targets[i] * np.dot(input, weights) > 0):
             num_correct += 1
-    return (num_correct / n) * 100
+    return (num_correct / N) * 100
 
-# Learning
-#weights = np.random.randn(2)# Initiate weights by random
-weights = np.random.randn(3)
-print('Initial weights :', weights)
-print('Initial weights accuracy : ', percent_correct(data_train, label_train, weights))
-
-num_train = 200
-iteration = 1000
-learning_rate = 0.01
-
-accuracy_train = np.zeros(iteration)
-accuracy_test = np.zeros(iteration)
-
-for i in range(iteration):
-    random_index = np.floor(np.random.rand() * num_train).astype(int)
-    sample = data_train[random_index,:]
-    
-    # If misclassified, adjust the weight based on how much the inaccuracy is
-    if(label_train[random_index] * np.dot(sample, weights) < 0):
-        weights += learning_rate * label_train[random_index] * sample
-        
-    accuracy_train[i] = percent_correct(data_train, label_train, weights)
-    accuracy_test[i] = percent_correct(data_test, label_test, weights)
-    
-plt.plot(range(iteration), accuracy_train, c = 'r', label = 'accuracy_train')
-plt.plot(range(iteration), accuracy_test, c = 'g', label = 'accuracy_test')
-plt.legend()
-
-print('Final weights : ', weights)
-print('Final weights train accuracy : ', accuracy_train[-1])
-print('Final weights test accuracy : ', accuracy_test[-1])
-
-# Perceptron using scikit-learn
-classifier = Perceptron()
-classifier.fit(data_train, label_train)
-train_predict = classifier.predict(data_train)
-test_predict = classifier.predict(data_test)
-print('Scikit-learn train accuracy : ', accuracy_score(train_predict, label_train) * 100)
-print('Scikit-learn test accuracy : ', accuracy_score(test_predict, label_test) * 100)
+#%% Testing with benchmark datasets
+iris_data = pd.read_csv('/home/tpanyapiang/git/MSc/foundations_of_ml/Lab2/iris.data',
+                        names=['sepal_length_cm', 'sepal_width_cm', 'petal_length_cm', 'petal_width_cm', 'class'])
+wine_data = pd.read_csv('/home/tpanyapiang/git/MSc/foundations_of_ml/Lab2/wine.data',
+                        names=['class','alcohol','malic_acid','ash','alcalinity_of_ash','magnesium','total_phenols','flavanoids','nonflavanoid_phenols','proanthocyanins','color_intensity','hue','OD280_OD315','proline'])
