643794ff8470b7e6d06e181830640176c214270a
thanakorn <thanakorn.panyapiang@outlook.com>
2019-11-18 16:19:43 +0000

    Finish Lab 5 report

--- Lab5/Lab5.py
+++ Lab5/Lab5.py
@@ -7,6 +7,7 @@ from sklearn.preprocessing import normalize, scale
 from sklearn.cluster import KMeans
 from sklearn.metrics import mean_squared_error
 from sklearn.svm import SVR
+from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import RBF
 %matplotlib inline
 plt.rcParams.update(plt.rcParamsDefault)
@@ -27,7 +28,7 @@ num_data, num_features = diabetes_data.shape
 train_data, test_data, train_target, test_target = train_test_split(diabetes_data, diabetes_target, test_size=0.3)
 
 # Design matrix
-M = 30
+M = 200
 
 # Basis function = distance between two random data
 C = np.random.randn(M, num_features)
@@ -68,13 +69,11 @@ ax[1].text(0.75, 0.1, f'Mean Sqr Error = %.2f' % mean_squared_error(test_target,
 
 # %% RBF Improvement
 # Normalized features
-normalized_diabetes_data = diabetes_data
-for i in range(num_features):
+normalized_diabetes_data = np.copy(diabetes_data)
+for i in range(num_features - 1):
     feature = diabetes_data[:,i]
     normalized_feature = scale(feature, axis = 0)
-    # print('--------')
-    # print('Mean : ', normalized_feature.mean())
-    # print('SD : ', normalized_feature.std())
+    normalized_diabetes_data[:,i] = normalized_feature
 
 # Adjusting sigma based on several data points
 num_data_points = 50
@@ -88,7 +87,7 @@ avg_sigma = distances.mean()
 C_cluster = KMeans(n_clusters=M).fit(normalized_diabetes_data).cluster_centers_
 
 # Finding new coefficients and predict
-M_new = 30
+M_new = 20
 train_data, test_data, train_target, test_target = train_test_split(normalized_diabetes_data, diabetes_target, test_size=0.3)
 
 num_data_train = train_data.shape[0]
@@ -126,9 +125,11 @@ ax[1].text(0.75, 0.1, f'Mean Sqr Error = %.2f' % mean_squared_error(test_target,
 
 # %% 10-Fold Cross-validation
 K = 10
-M = 30
+M = 20
 C = KMeans(n_clusters=M).fit(normalized_diabetes_data).cluster_centers_
 kfold = KFold(n_splits=K)
+rbf_errors = np.zeros(K)
+lr_errors = np.zeros(K)
 round = 0
 for train_indices, test_indices in kfold.split(normalized_diabetes_data):
     train_data = normalized_diabetes_data[train_indices]
@@ -149,71 +150,53 @@ for train_indices, test_indices in kfold.split(normalized_diabetes_data):
     predict_train = U_train @ w_rbf
     predict_rbf = U_test @ w_rbf
     predict_lr = test_data @ w_lr
+    rbf_err = mean_squared_error(test_target, predict_rbf)
+    lr_err = mean_squared_error(test_target, predict_lr)
+    rbf_errors[round] = rbf_err
+    lr_errors[round] = lr_err
     fig, ax = plt.subplots(nrows=1, ncols=2, figsize = (12,3))
     ax[0].grid(True)
     ax[0].set_title('Linear Regression')
     ax[0].set_xlabel('Actual')
     ax[0].set_ylabel('Predict')
-    ax[0].text(0.75, 0.1, f'Mean Sqr Error = %.2f' % mean_squared_error(test_target, predict_lr), weight = 'bold', horizontalalignment='center',verticalalignment='center', transform=ax[0].transAxes)
+    ax[0].text(0.75, 0.1, f'Mean Sqr Error = %.2f' % lr_err, weight = 'bold', horizontalalignment='center',verticalalignment='center', transform=ax[0].transAxes)
     ax[0].scatter(test_target, predict_lr)
     ax[1].grid(True)
     ax[1].set_title('RBF')
     ax[1].set_xlabel('Actual')
     ax[1].set_ylabel('Predict')
-    ax[1].text(0.75, 0.1, f'Mean Sqr Error = %.2f' % mean_squared_error(test_target, predict_rbf), weight = 'bold', horizontalalignment='center',verticalalignment='center', transform=ax[1].transAxes)
+    ax[1].text(0.75, 0.1, f'Mean Sqr Error = %.2f' % rbf_err, weight = 'bold', horizontalalignment='center',verticalalignment='center', transform=ax[1].transAxes)
     ax[1].scatter(test_target, predict_rbf, color='r')
     # plt.savefig(f'Lab5/train_test_perf_%d' % (round+1))
 
     fig, ax = plt.subplots(figsize=(3,3))
     ax.grid(True)
     ax.boxplot([test_target, predict_lr, predict_rbf], labels=['Actual', 'Linear\nRegression', 'RBF'])
-    plt.savefig(f'Lab5/result_dist_%d' % (round+1), bbox_inches='tight')
-
+    # plt.savefig(f'Lab5/result_dist_%d' % (round+1), bbox_inches='tight')
     round += 1
 
+#%%
+fig, ax = plt.subplots(figsize=(3,3))
+ax.set_title('Errors on Test set')
+ax.grid(True)
+ax.boxplot([lr_errors, rbf_errors], labels=['Linear\nRegression', 'RBF'])
+# plt.savefig('Lab5/error_compare', bbox_inches='tight')
+
 # %% Compare with scikit-learn RBF
-# for train_indices, test_indices in kfold.split(normalized_diabetes_data):
-#     train_data = normalized_diabetes_data[train_indices]
-#     train_target = diabetes_target[train_indices]
-#     test_data = normalized_diabetes_data[test_indices]
-#     test_target = diabetes_target[test_indices]
-#     U_train = np.zeros((len(train_indices), M))
-#     U_test = np.zeros((len(test_data), M))
-#     for i in range(len(train_indices)):
-#         for j in range(M):
-#             U_train[i,j] = gaussian(train_data[i,:], C[j,:], avg_sigma)
-#     for i in range(len(test_indices)):
-#         for j in range(M):
-#             U_test[i,j] = gaussian(test_data[i,:], C[j,:], avg_sigma)
-
-#     w_rbf = np.linalg.inv(U_train.T @ U_train) @ U_train.T @ train_target
-#     svr_rbf = SVR(kernel='rbf', gamma='scale')
-#     svr_rbf.fit(train_data, train_target)
-#     predict_rbf = U_test @ w_rbf
-#     predict_svr = svr_rbf.predict(test_data)
-#     fig, ax = plt.subplots(nrows=1, ncols=3, figsize = (18,3))
-#     ax[0].grid(True)
-#     ax[0].set_title('RBF')
-#     ax[0].set_xlabel('Actual')
-#     ax[0].set_ylabel('Predict')
-#     ax[0].text(0.75, 0.1, f'Mean Sqr Error = %.2f' % mean_squared_error(test_target, predict_rbf), weight = 'bold', horizontalalignment='center',verticalalignment='center', transform=ax[0].transAxes)
-#     ax[0].scatter(test_target, predict_rbf)
-#     ax[1].grid(True)
-#     ax[1].set_title('scikit-learn')
-#     ax[1].set_xlabel('Actual')
-#     ax[1].set_ylabel('Predict')
-#     ax[1].text(0.75, 0.1, f'Mean Sqr Error = %.2f' % mean_squared_error(test_target, predict_svr), weight = 'bold', horizontalalignment='center',verticalalignment='center', transform=ax[1].transAxes)
-#     ax[1].scatter(test_target, predict_svr, color='r')
-#     ax[2].grid(True)
-#     ax[2].set_title('Result Distributions')
-#     ax[2].boxplot([predict_rbf, predict_svr], labels=['RBF', 'sklearn'])
-
-train_data, test_data, train_target, test_target = train_test_split(diabetes_data, diabetes_target, test_size=0.3)
-svr_rbf = SVR(kernel='rbf', gamma='scale')
+train_data, test_data, train_target, test_target = train_test_split(normalized_diabetes_data, diabetes_target, test_size=0.3)
+svr_rbf = SVR(kernel='rbf', gamma='scale', epsilon=100.0)
 svr_rbf.fit(train_data, train_target)
-predict_svr = svr_rbf.predict(train_data)
-fig, ax = plt.subplots(figsize = (3,3))
-ax.set_ylim((0,300))
-ax.scatter(train_target, predict_svr)
+predict_svr_train = svr_rbf.predict(train_data)
+predict_svr_test = svr_rbf.predict(test_data)
 
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(12,3))
+ax[0].set_title('Training Set')
+ax[0].set_ylim((0,300))
+ax[0].scatter(train_target, predict_svr_train)
+ax[0].text(0.75, 0.1, f'Mean Sqr Error = %.2f' % mean_squared_error(train_target, predict_svr_train), weight = 'bold', horizontalalignment='center',verticalalignment='center', transform=ax[0].transAxes)
+ax[1].set_title('Test Set')
+ax[1].set_ylim((0,300))
+ax[1].scatter(test_target, predict_svr_test, color='r')
+ax[1].text(0.75, 0.1, f'Mean Sqr Error = %.2f' % mean_squared_error(test_target, predict_svr_test), weight = 'bold', horizontalalignment='center',verticalalignment='center', transform=ax[1].transAxes)
+plt.savefig('Lab5/sklearn_compare', bbox_inches='tight')
 # %%
